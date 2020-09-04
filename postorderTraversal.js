6a8d63c4762c2a063a9436ded11d1b01330711cb
leo2436 <chenll1996@foxmail.com>
2020-09-04 21:55:12 +0800

    postorder

--- postorderTraversal.js
+++ postorderTraversal.js
@@ -1,3 +1,4 @@
+//递归
 var postorderTraversal = function (root) {
   function traversal(root, stack) {
     if (!root) return stack;
@@ -8,3 +9,18 @@ var postorderTraversal = function (root) {
   }
   return traversal(root, [])
 };
+
+
+//迭代
+function postorderTraversal(root) {
+  let stack = [root];
+  let res = [];
+  while (stack.length) {
+    let curr = stack.pop();
+    if (!curr) return stack;
+    res.unshift(curr.val);
+    if (curr.left) stack.push(curr.left);
+    if (curr.right) stack.push(curr.right);
+  }
+  return res;
+}
