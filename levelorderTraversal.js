9dbf7e10c6e64ed301f9f75dc19fce4dfae134f7
leo2436 <chenll1996@foxmail.com>
2020-09-06 22:24:17 +0800

    levelorder

--- levelorderTraversal.js
+++ levelorderTraversal.js
@@ -1,3 +1,4 @@
+//迭代bfs
 var levelOrder = function (root) {
   let res = [];
   let queue = [];
@@ -15,3 +16,40 @@ var levelOrder = function (root) {
   }
   return res;
 };
+
+function levelOrder(root) {
+  let res = [[]];
+  if (!root) return [];
+  function dfs(node, level) {
+    if (res.length === level) {
+      res.push([]);
+    }
+    res[level].push(node.val);
+    if (node.left) dfs(node.left, level + 1);
+    if (node.right) dfs(node.right, level + 1);
+  }
+  dfs(root, 0);
+  return res;
+}
+
+//递归
+function levelOrder(root) {
+  let res = [[]];
+  if (!root) return [];
+  function dfs(node, level) {
+    if (res.length === level) {
+      res.push([]);
+    }
+    res[level].push(node.val);
+    if (node.left) dfs(node.left, level + 1);
+    if (node.right) dfs(node.right, level + 1);
+  }
+  dfs(root, 0);
+  return res;
+}
+
+
+// let root = { val: 1, left: { val: 2, left: { val: 4, left: null, right: null }, right: { val: 5, left: null, right: null } }, right: { val: 3, left: null, right: null } }
+
+// let res = levelOrder(root);
+// console.log('res: ', res);
