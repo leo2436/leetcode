6f485d7f9db463d14f724ed3ddc357824ffcdc54
leo2436 <chenll1996@foxmail.com>
2021-03-10 22:12:41 +0800

    merge

--- merge.js
+++ merge.js
@@ -4,6 +4,11 @@
 
 var merge = function (nums1, m, nums2, n) {
   while (n > 0) {
+    if (m <= 0) {
+      nums1[m + n - 1] = nums2[n - 1];
+      n--;
+      continue;
+    }
     if (nums1[m - 1] >= nums2[n - 1]) {
       nums1[m + n - 1] = nums1[m - 1];
       m--;
