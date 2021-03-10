0a9443496b7ac8142a0109d2151b9b300b00b441
leo2436 <chenll1996@foxmail.com>
2021-03-10 22:02:26 +0800

    merge

---
+++ merge.js
@@ -0,0 +1,16 @@
+// 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+var merge = function (nums1, m, nums2, n) {
+  while (n > 0) {
+    if (nums1[m - 1] >= nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[m - 1];
+      m--;
+    } else {
+      nums1[m + n - 1] = nums2[n - 1];
+      n--;
+    }
+  }
+  return nums1;
+};
