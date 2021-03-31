7a38d9f64eeba4b7611e55633ddc0095020acb63
leo2436 <chenll1996@foxmail.com>
2021-03-31 20:53:32 +0800

    master

--- maxSlidingWindow.js
+++ maxSlidingWindow.js
@@ -1,3 +1,5 @@
+// 双端队列：维持一个单调递减的队列，入队数大于队尾数则弹出队尾数。
+// 队首数离开滑动窗口，则出队。
 var maxSlidingWindow = function (nums, k) {
   let queue = [];
   let res = [];
@@ -31,4 +33,25 @@ var maxSlidingWindow = function (nums, k) {
   }
   return res;
 };
+
+// 简化
+var maxSlidingWindow = function (nums, k) {
+  const deque = [];
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    // 把滑动窗口之外的踢出
+    if (i - deque[0] >= k) {
+      deque.shift();
+    }
+    while (nums[deque[deque.length - 1]] <= nums[i]) {
+      deque.pop();
+    }
+    deque.push(i);
+    if (i >= k - 1) {
+      result.push(nums[deque[0]]);
+    }
+  }
+  return result;
+};
+
 var res = maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3);
